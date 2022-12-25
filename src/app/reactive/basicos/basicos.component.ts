9176ccbbb727283b3a9f4db7e45f8a035574f4e1
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-25 13:15:09 -0500

    Iniciando de nuevo

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
-  templateUrl: './basicos.component.html'
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre'     : new FormControl('RTX 4080ti'),
+  //   'precio'     : new FormControl(1500),
+  //   'existencias': new FormControl(5),
+  // })
 
-  ngOnInit(): void {
-  }
+  miFormulario: FormGroup = this.fb.group({
+    nombre     : [ 'RTX 4080ti', ,  ],
+    precio     : [ 0 ],
+    existencias: [ 0 ],
+  })
+
+  constructor( private fb: FormBuilder ) { }
+
+
 
 }
